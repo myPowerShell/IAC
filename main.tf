85582d7d8dbc199bb9f8c570986dbf5963d8b8b9
My PowerShell <39554666+myPowerShell@users.noreply.github.com>
2021-08-11 11:46:51 -0400

    commit "update"

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
-# Configure the Azure provider
 terraform {
+
+  required_version = ">=0.12"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.65"
+      version = "~>2.0"
     }
   }
-
-  required_version = ">= 0.14.9"
 }
 
 provider "azurerm" {
@@ -15,6 +15,6 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "myTFResourceGroup"
-  location = "eastus2"
+  name     = var.resource_group_name
+  location = var.resource_group_location
 }
